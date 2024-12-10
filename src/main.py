f4d42603b491ef48e68e2b00dc48a9a5aa2d576c
Z3eYY <yiyangzhang9@gmail.com>
2024-12-09 18:07:41 -0700

    1

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix,classification_report
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier
 import argparse
@@ -13,39 +12,18 @@ val_data = pd.read_csv('data/val_data.csv')
 test_data = pd.read_csv('data/test_data.csv')
 
 # Initialize CountVectorizer for Bag-of-Words
-'''
-Initializes the Bag-of-Words model.
-max_features=5000: Limits the vocabulary to the 5,000 most frequent words across the dataset, reducing dimensionality.
-stop_words='english': Automatically removes common English stop words (e.g., "the," "and," "is") to focus on meaningful words.
-'''
+
 bow_vectorizer = CountVectorizer(max_features=5000, stop_words='english')
 
 
-
-# Fit on training data and transform
-'''
-bow_vectorizer.fit_transform(train_data['text']):
-
-Fit: Learns the vocabulary from the training data.
-Transform: Converts the text data (train_data['text']) into a sparse matrix of word counts.
-X_train: Numerical representation of the training text data.
-y_train = train_data['sentiment']:
-
-Extracts the target labels (sentiments) from the training dataset to form y_train.
-'''
 X_train = bow_vectorizer.fit_transform(train_data['text'])
 y_train = train_data['sentiment']
 
-# Transform validation and test data
 X_val = bow_vectorizer.transform(val_data['text'])
 y_val = val_data['sentiment']
 
 X_test = bow_vectorizer.transform(test_data['text'])
 y_test = test_data['sentiment']
-
-# Save the feature names (optional, for understanding BoW output)
-feature_names = bow_vectorizer.get_feature_names_out()
-
 
 
 # Logistic model #
@@ -80,7 +58,6 @@ def training_logistic(x, y,learning_rate=0.01):
             b -= learning_rate * db
 
     return w, b
-
 
 
 def predict_logistic(X,w,b):
@@ -130,7 +107,6 @@ def predict_naive_bayes(X,logprior,loglikelihood,classes):
 
 
 
-
 def evaluate(y_true, y_pred):
     acc = accuracy_score(y_true, y_pred)
     precision = precision_score(y_true, y_pred)
@@ -161,12 +137,12 @@ def main():
                         help="Choose which model to run: logistic, naive_bayes, random_forest, svm")
     args = parser.parse_args()
 
-    # Step 1: Prepare the Data
+  
     print("Preparing data...")
-    # Convert the sparse matrix to dense and extract labels
-    X_train_dense = X_train.toarray()
-    X_val_dense = X_val.toarray()
-    X_test_dense = X_test.toarray()
+
+    X_tarin_arr = X_train.toarray()
+    X_val_arr = X_val.toarray()
+    X_test_arr = X_test.toarray()
 
     y_train_np = y_train.to_numpy()
     y_val_np = y_val.to_numpy()
@@ -187,11 +163,11 @@ def main():
     if args.model == 'logistic':
     
         print("Training logistic regression model...\n")
-        w, b = training_logistic(X_train_dense, y_train_np,learning_rate=0.01)
+        w, b = training_logistic(X_tarin_arr, y_train_np,learning_rate=0.01)
 
         print("Making predictions...\n")
-        y_val_pred = predict_logistic(X_val_dense, w, b)
-        y_test_pred = predict_logistic(X_test_dense, w, b)
+        y_val_pred = predict_logistic(X_val_arr, w, b)
+        y_test_pred = predict_logistic(X_test_arr, w, b)
 
       # Evaluate
         print("logistic Validation Set:")
@@ -208,13 +184,13 @@ def main():
     elif args.model == 'naive_bayes':
    
         print("Training Naive Bayes model...\n")
-        logprior, loglikelihood, vocabulary, classes = train_naive_bayes(X_train_dense, y_train_np)
+        logprior, loglikelihood, vocabulary, classes = train_naive_bayes(X_tarin_arr, y_train_np)
 
         print("Making predictions...\n")
     
-        y_val_pred = predict_naive_bayes(X_val_dense, logprior, loglikelihood, classes)
+        y_val_pred = predict_naive_bayes(X_val_arr, logprior, loglikelihood, classes)
     
-        y_test_pred = predict_naive_bayes(X_test_dense, logprior, loglikelihood, classes)
+        y_test_pred = predict_naive_bayes(X_test_arr, logprior, loglikelihood, classes)
 
       # Evaluate
         print("Naive Bayes Validation Set:")
@@ -230,18 +206,18 @@ def main():
     elif args.model == 'random_forest':
         print("\nTraining Random forest model...\n")
         forest = RandomForestClassifier(
-            n_estimators=100,      # Number of trees in the forest
-            max_depth=20,          # Maximum depth of the trees
-            max_features='sqrt',   # Number of features to consider for each split
-            random_state=42,       # Random seed for reproducibility
-            class_weight='balanced', # Handle class imbalance
-            n_jobs=-1              # Use all available CPU cores
+            n_estimators=100,     
+            max_depth=20,        
+            max_features='sqrt', 
+            random_state=42,       
+            class_weight='balanced', 
+            n_jobs=-1              
         )
 
-        forest.fit(X_train_dense, y_train_np)
+        forest.fit(X_tarin_arr, y_train_np)
 
-        y_val_pred = forest.predict(X_val_dense)
-        y_test_pred = forest.predict(X_test_dense)
+        y_val_pred = forest.predict(X_val_arr)
+        y_test_pred = forest.predict(X_test_arr)
 
 
       # Evaluate
@@ -257,16 +233,17 @@ def main():
     elif args.model == 'svm':
         print("Training svm ...")
         svm = LinearSVC(C=1.0, random_state=42)
-        svm.fit(X_train_dense, y_train_np)
+        svm.fit(X_tarin_arr, y_train_np)
 
         # Predict
-        y_val_pred = svm.predict(X_val_dense)
-        y_test_pred = svm.predict(X_test_dense)
+        y_val_pred = svm.predict(X_val_arr)
+        y_test_pred = svm.predict(X_test_arr)
 
 
         # Evaluate
         print("svm Validation Set:")
         print(classification_report(y_val_np, y_val_pred))
+
         print("Confusion Matrix:\n", confusion_matrix(y_val_np, y_val_pred))
 
         print("\nTest Set:")
