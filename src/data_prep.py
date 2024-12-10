2ed47c8a3e23172e51fd51a7c6a59e77623a799e
Z3eYY <yiyangzhang9@gmail.com>
2024-12-09 18:23:54 -0700

    1

--- src/data_prep.py
+++ src/data_prep.py
@@ -29,15 +29,17 @@ data.drop_duplicates(inplace=True)
 
 #conver text to lower
 data['text'] = data['text'].str.lower()
+
+# shuffle and reduced to 10% of original data
 data = data.sample(frac=1, random_state=28)
 reduced_data = data.sample(frac=0.1, random_state=42)
 cleaned_file_path = 'data/cleaned_data.csv'
 reduced_data.to_csv(cleaned_file_path, index=True, index_label="index")
 
 
-
+# split out training set
 train_data, temp_data = train_test_split(reduced_data, test_size=0.3, random_state=38)
-
+# split out test and validation set
 val_data, test_data = train_test_split(temp_data, test_size=0.5, random_state=38)
 
 
@@ -46,11 +48,9 @@ train_data.dropna(subset=['text'], inplace=True)
 val_data.dropna(subset=['text'], inplace=True)
 test_data.dropna(subset=['text'], inplace=True)
 
-# Ensure all text values are strings (if necessary)
 train_data['text'] = train_data['text'].astype(str)
 val_data['text'] = val_data['text'].astype(str)
 test_data['text'] = test_data['text'].astype(str)
-
 
 
 # Remove rows with empty strings or only whitespace
@@ -59,17 +59,10 @@ val_data = val_data[val_data['text'].str.strip() != '']
 test_data = test_data[test_data['text'].str.strip() != '']
 
 
-
-
 train_data.to_csv('data/train_data.csv',index_label="index")
 val_data.to_csv('data/val_data.csv',index_label="index")
 test_data.to_csv('data/test_data.csv',index_label="index")
 
-
-print("Data successfully split and saved:")
-print(f"Training data: {len(train_data)} samples")
-print(f"Validation data: {len(val_data)} samples")
-print(f"Test data: {len(test_data)} samples")
 
 
 
